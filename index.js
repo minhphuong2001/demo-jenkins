5a11de18f2af11dff3eb1294ea15c2af892fcc98
Minh Phuong Do <minhphuongdo2001@gmail.com>
2021-10-10 08:55:19 +0700

    change port

--- index.js
+++ index.js
@@ -11,4 +11,6 @@ app.use(express.json());
 
 router(app);
 
-app.listen(5000, console.log(`Server running on PORT 5000`));
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`Server running on PORT 5000`));
